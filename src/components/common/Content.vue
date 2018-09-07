0fffdfa7bc5b257f41f3545ba98861bca2d9ccfd
GodRuffian <1019962774@qq.com>
2018-09-07 19:07:57 +0800

    初始化项目结构

---
+++ src/components/common/Content.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+
+        },
+        data() {
+
+        },
+        components: {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style scoped="scss" rel="stylesheet/scss">
+
+</style>
